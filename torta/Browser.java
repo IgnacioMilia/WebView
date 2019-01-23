e8a8b16281e06615aa3f0093800eda3e05a38ddd
Ignacio Milia <ignaciomilia91@gmail.com>
2019-01-23 13:51:12 -0300

    WebView

---
+++ torta/Browser.java
@@ -0,0 +1,34 @@
+package torta;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Browser extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        root.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
+        Scene scene = new Scene(root/*200,200*/);
+        primaryStage.setTitle("WebView");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ torta/MainController.java
@@ -0,0 +1,35 @@
+package torta;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+public class MainController implements Initializable{
+    
+    @FXML private WebView webView;
+    private WebEngine engine; 
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        engine = webView.getEngine();
+    }
+    
+    public void btn1(ActionEvent evt){
+        engine.load("https://www.google.com");
+    }
+    
+    public void btn2(ActionEvent evt){
+        engine.executeScript("window.location = \"https://www.youtube.com/watch?v=t4ehYIynI34//\";");
+    }
+    public void btn3(ActionEvent evt){
+        engine.loadContent("<html><body><h1>Hello</h1></body></html>");
+    }
+    public void btn4(ActionEvent evt){
+        engine.reload();
+    }
+    
+}
